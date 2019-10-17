b20e70ff094b04536ea9c36eecff33b94ab580da
Mariano Paz <paz.mariano@gmail.com>
2019-10-17 14:50:44 -0300

    Fix

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "aws_batch_job_definition" "batch_job_definition" {
   name = "${var.name}"
   type = "${var.type}"
 
-  container_properties = ${var.container_properties}
+  container_properties = "${var.container_properties}"
 }
