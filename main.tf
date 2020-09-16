c28fa364c56977f6e8eb2a88a2b7fe9cdc42ac73
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-09-16 19:12:59 -0300

    upgrade to terraform 0.12

--- main.tf
+++ main.tf
@@ -1,16 +1,17 @@
 resource "aws_batch_job_definition" "batch_job_definition" {
-  name = "${var.name}"
-  type = "${var.type}"
+  name = var.name
+  type = var.type
 
-  container_properties = "${var.container_properties}"
+  container_properties = var.container_properties
 
-  parameters = "${var.parameters}"
+  parameters = var.parameters
 
-  retry_strategy = {
-    attempts = "${var.attempts}"
+  retry_strategy {
+    attempts = var.attempts
   }
 
-  timeout = {
-    attempt_duration_seconds = "${var.attempt_duration_seconds}"
+  timeout {
+    attempt_duration_seconds = var.attempt_duration_seconds
   }
 }
+
--- variables.tf
+++ variables.tf
@@ -1,30 +1,30 @@
 ##Variables for Batch Job Definition
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Specifies the name of the job definition"
 }
 
 variable "container_properties" {
-  type        = "string"
+  type        = string
   description = "A valid container properties provide as a single valid JSON document. This parameters is required if the type parameter is container"
 }
 
 variable "parameters" {
-  type        = "map"
+  type        = map(string)
   description = "Specifies the parameters substitution placeholders to set in the job definition"
   default     = {}
 }
 
 variable "type" {
-  type        = "string"
+  type        = string
   description = "The type of job definition. Must be container"
 }
 
 ###Variable for retry_strategy
 
 variable "attempts" {
-  type        = "string"
+  type        = string
   description = "The number of time to move a job to the RUNNABLE status. You may specify between 1 and 10 attemps"
   default     = "1"
 }
@@ -32,7 +32,8 @@ variable "attempts" {
 ##Variable for timeout
 
 variable "attempt_duration_seconds" {
-  type        = "string"
+  type        = string
   description = "The time duration in seconds after which AWS Batch terminates your jobs if they have not finished. The minimum value for the timeout is 60 seconds"
   default     = "60"
-} 
+}
+
